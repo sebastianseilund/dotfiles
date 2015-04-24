9c27be935106926e85079a1ac364f671c5b59342
Sebastian Seilund <sebsei@gmail.com>
2015-04-24 11:30:08 -0700

    Altered gp and gpu git push aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,7 +10,8 @@ fi
 alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push'
+alias gpu='git push -u origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
 alias gca='git commit -a'
