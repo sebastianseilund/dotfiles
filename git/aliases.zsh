c485dc043e381f7c301b06267eb8830f434eef8f
Sebastian Seilund <sebsei@gmail.com>
2015-04-27 11:07:00 -0700

    Added gpt alias to git push and git push --tags

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,6 +11,7 @@ alias g='git'
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push'
+alias gpt='git push && git push --tags'
 alias gpu='git push -u origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
